e543fc49334b4d5355f7166b01ed9c41aaa00b2c
ncr00t <r.ivanisenko@gmail.com>
2019-09-23 11:56:15 +0400

    added update positions for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     unless @users.blank?
       amount = (params[:amount].blank?) ? @users.size
                                         : params[:amount].to_i
-      top_users = sort_users_by_desc(@users).take(amount)
+      update_all_user_positions
+      top_users = sort_users_by_desc.take(amount)
       top_users_with_current = top_users.include?(@current_user) ? top_users :
                                                                    top_users.take(amount - 1)
                                                                             .push(@current_user)
@@ -25,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      update_user_position(@user)
       render json: @user, status: :created
     else
       render json: { errors: @user.errors.full_messages },
@@ -47,8 +49,9 @@ class UsersController < ApplicationController
 
   def set_steps
     if current_user?(@user)
-       @current_user.update(steps_param)
-      render json: @current_user, status: :ok
+       @user.update(steps_param)
+       update_user_position(@user)
+      render json: @user, status: :ok
     else
       render json: { errors: 'No rights to change' },
              status: :unprocessable_entity
@@ -64,8 +67,8 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    defaults = {steps: Random.rand(100...5000),
-                position: (@users.blank?) ? 1 : 0}
+    @users = User.all
+    defaults = {steps: Random.rand(100...5000), position: (@users.blank?) ? 1 : 0}
     params.permit(
         :name, :email, :password, :password_confirmation
     ).reverse_merge(defaults)
@@ -75,27 +78,37 @@ class UsersController < ApplicationController
     params.permit(:steps)
   end
 
-  def steps_update
-    if Time.now > Time.now + 2.seconds.to_i
-      @current_user.update(steps: 0)
-    end
-  end
-
-  def sort_users_by_desc(users)
-    unless users.blank?
+  def sort_users_by_desc
+    @users = User.all
+    unless @users.blank?
       sorted_users = @users.sort { |current_user, next_user |
         next_user[:steps] <=> current_user[:steps]
       }
-      assign_positions_for_users(sorted_users)
+      assign_user_positions(sorted_users)
     end
   end
 
-  def assign_positions_for_users(sorted_users)
+  def assign_user_positions(sorted_users)
     position = 0
     sorted_users.each do |user|
       position += 1
       user[:position] = position
     end
-    sorted_users
+  end
+
+  def update_all_user_positions
+    sort_users_by_desc.each { |user| user.update(position: user[:position]) }
+  end
+
+  def get_user_position(user_id)
+    sort_users_by_desc.each do |user|
+      if user[:id] == user_id
+        return user[:position]
+      end
+    end
+  end
+
+  def update_user_position(user)
+    user.update(position: get_user_position(user.id))
   end
 end
